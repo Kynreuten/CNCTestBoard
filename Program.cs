45c2d0785d0168167bf6a9eb8eb4fe34f35cf0ce
Chris Golden <kyndig2@gmail.com>
2021-08-09 11:18:30 -0500

    Setup to be able to run

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+using GoldenLlama.Cnc.Test;
+
+namespace CNCTestBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generating G-Code test!");
+
+            var tester = new BackAndForthRateTester
+            {
+                FeedRateInitial = 4.87,
+                // FeedRateIncrement = ?,
+                TestDistance = 2.5,
+            };
+
+            string operations = tester.GenerateTest();
+
+            string targetFilePath = Path.Combine(Environment.CurrentDirectory,
+                                                    "Generated Tests",
+                                                    "BackAndForthRateTester.nc");
+            var fileInfo = new FileInfo(targetFilePath);      
+            if (!fileInfo.Directory.Exists) {
+                fileInfo.Directory.Create();
+            }
+            using (var output = new StreamWriter(fileInfo.OpenWrite()))
+            {
+                output.WriteLine(operations);
+            }
+            Console.WriteLine(operations);
+        }
+    }
+}
