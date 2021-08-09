1e09a9459b04f2c6ec9090b19ed0c669674709c8
Chris Golden <kyndig2@gmail.com>
2021-08-09 10:01:50 -0500

    Vector now provides many useful constructors

--- Manual Tests/src/Vector.cs
+++ Manual Tests/src/Vector.cs
@@ -11,6 +11,23 @@ namespace GoldenLlama.Cnc
 
         public double? FeedRate { get; set; }
 
+        public Vector() { }
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Vector(double x, double y, double z, double feedRate)
+            : this(x, y, z)
+        {
+            FeedRate = feedRate;
+        }
+        public Vector(Vector source)
+            : this(source.X, source.Y, source.Z, source.FeedRate)
+        {
+        }
+
 
         public override string ToString()
         {
